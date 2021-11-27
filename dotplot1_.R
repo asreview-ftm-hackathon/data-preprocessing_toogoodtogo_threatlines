416e11417b1f3257388e1834001c2c5b07ea196b
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-11-27 15:44:41 +0100

    Correct dotplot1_.R

--- dotplot1_.R
+++ dotplot1_.R
@@ -3,15 +3,21 @@
 install.packages("readr")
 library("readr")
 library("dplyr")
+library("stringr")
 library("ggplot2")
 
 fn <- file.path("data", "split_emails.csv")
 data <- read_delim(fn, delim = "|")
 
-mails <- data %>% filter(type == "Mail")
+mail <- data %>% filter(type == "Mail") %>%
+  filter(betterDate >= ymd(20140101) & betterDate < ymd(20200101)) %>%
+  group_by(id) %>%
+  mutate(thread_length = n()) %>%
+  filter(thread_length > 1)
 
-g <- ggplot(mails, aes(x = betterDate, y = reorder(title, betterDate))) +
-  geom_point(size = 2) +
-  theme_light() +
-  theme(axis.text.x = element_text(angle = 60, hjust = 1))
+g <- ggplot(mail, aes(x = betterDate, y = reorder(id, betterDate))) +
+  geom_point(size = 1) +
+  theme_linedraw() +
+
+  theme(axis.text.x = element_text(angle = 0, hjust = 1))
 
