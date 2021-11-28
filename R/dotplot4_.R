f3559e0a58d99894f450d2b7b36528a5e76d1975
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-11-28 10:59:46 +0100

    Add dotplot4

---
+++ R/dotplot4_.R
@@ -0,0 +1,27 @@
+# script to read data
+
+# install.packages("readr")
+# install.packages("lubridate")
+# install.packages("ggplot2")
+# install.packages("plotly")
+library("readr")
+library("dplyr")
+library("stringr")
+library("ggplot2")
+library("plotly")
+
+fn <- file.path("data", "split_emails.csv")
+data <- read_delim(fn, delim = "|")
+
+mail <- data %>% filter(doc_type == "Mail") %>%
+  filter(betterDate >= ymd(20140101) & betterDate < ymd(20200101)) %>%
+  group_by(doc_id) %>%
+  mutate(thread_length = n()) %>%
+  filter(thread_length > 1)
+
+g <- ggplot(mail, aes(x = betterDate, y = reorder(doc_id, betterDate))) +
+  geom_point(size = 0.7, aes(text = title)) +
+  theme_classic()
+p <- ggplotly(g)
+p
+
